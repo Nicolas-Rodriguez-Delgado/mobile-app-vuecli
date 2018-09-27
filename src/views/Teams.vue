770442285bb78648dde2561cd184923cdc0923b3
Nicolas-Rodriguez-Delgado <spastikcitizen@gmail.com>
2018-09-27 10:40:25 +0200

    "carrousel item in teams view implemented"

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,26 +1,14 @@
-<template>
     
-    <div class="container">
+    <!-- <div class="container">
         <div class="row">
+            <div class="col">
+                <img style="width:90%;" :src="myTeams[4].logo">
+            </div>
             <div class="col">
                 <img :src="myTeams[0].logo">
             </div>
             <div class="col">
-                <img :src="myTeams[1].logo">
-            </div>
-            <div class="col">
-                <img :src="myTeams[2].logo">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <img :src="myTeams[3].logo">
-            </div>
-            <div class="col">
-                <img :src="myTeams[4].logo">
-            </div>
-            <div class="col">
-                <img :src="myTeams[5].logo">
+                <img style="width:80%;" :src="myTeams[2].logo">
             </div>
         </div>
         <div class="row">
@@ -28,26 +16,43 @@
                 <img :src="myTeams[6].logo">
             </div>
             <div class="col">
-                <img :src="myTeams[7].logo">
+                <img :src="myTeams[1].logo">
             </div>
             <div class="col">
-                <img :src="myTeams[8].logo">
+                <img :src="myTeams[9].logo">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <img style="width:120%;" :src="myTeams[7].logo">
+            </div>
+            <div class="col">
+                <img style="width:130%;" :src="myTeams[5].logo">
+            </div>
+            <div class="col">
+                <img style="width:120%; " :src="myTeams[8].logo">
             </div>
         </div>
         <div class="row">
             <div class="col-4">
-                <img :src="myTeams[9].logo">
+                <img :src="myTeams[3].logo">
             </div>
             
         </div>
         
-    </div>
-    
+    </div> -->
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in myTeams"
+      :key="i"
+      :src="item.logo"
+    ></v-carousel-item>
+  </v-carousel>
 </template>
 <style>
-img {
-    width: 100%;
-    padding: 5px;
+.v-image__image--cover {
+    background-size: 270px 320px;
 }
 </style>
 
@@ -58,6 +63,7 @@ export default {
     data() {
         return {
             myTeams: null,
+            
         };
     },
     created() {
